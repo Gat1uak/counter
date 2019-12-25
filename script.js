0e7aba0adb579ff5d6422b94c657f99ed2a53dac
Gatluak Tholbok <tholbok@Gatluaks-MacBook-Pro.local>
2019-12-25 11:21:11 +1100

    added script.js functions

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Connection made!!");
+
+let counter = document.querySelector('.counter');
+const addCont = document.getElementById('addCounterBtn');
+const lowerCont = document.getElementById('lowerCounterBtn');
+
+let count = 0;
+
+addCont.addEventListener('click', incrementCounter);
+lowerCont.addEventListener('click', decrementCounter);
+
+function incrementCounter() {
+   count++;
+   counter.innerHTML = count;
+   if (counter.innerHTML > '0') {
+      counter.style.color = 'green';
+   } else if(counter.innerHTML === '0'){
+      counter.style.color = 'white';
+   }
+}
+function decrementCounter() {
+   count--;
+   counter.innerHTML = count;
+   if (counter.innerHTML < '0') {
+      counter.style.color = 'red';
+   } else if (counter.innerHTML === '0') {
+      counter.style.color = 'white';
+   }
+}
